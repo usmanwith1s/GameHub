e51ede043682fbd091a8a8dccc2c8cd429c46152
Muhammad Usman <usman589827@gmail.com>
2026-09-11 19:44:19 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
 
+    const themeToggle = document.querySelector(".theme-toggle");
+
+    if (themeToggle) {
+        themeToggle.addEventListener("click", () => {
+            document.body.classList.toggle("light-mode");
+        });
+    }
+
+});
