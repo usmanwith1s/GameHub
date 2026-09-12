96a3cf7df732e961d44e54567ebb20f9f8ba688d
Muhammad Usman <usman589827@gmail.com>
2026-09-13 02:05:29 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -71,6 +71,7 @@ document.addEventListener("DOMContentLoaded", () => {
     function savePlayer(username) {
 
         localStorage.setItem("gamehubPlayer", username);
+        updateLeaderboardPlayerNote();
 
         savedPlayer = username;
 
